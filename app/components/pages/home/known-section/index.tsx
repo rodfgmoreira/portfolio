6fffa69492a631a2691c81bd82c560d860317433
Rodrigo Moreira <rodfgmoreira@gmail.com>
2025-11-27 16:01:36 -0300

    feat: iniciado KnownTechs

---
+++ app/components/pages/home/known-section/index.tsx
@@ -0,0 +1,9 @@
+import { SectionTitle } from "@/app/components/section-title";
+
+export function KnownTechs() {
+  return (
+    <section className="container py-16">
+      <SectionTitle subtitle="competências" title="Conhecimentos" />
+    </section>
+  )
+}
--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,11 @@
 import { HeroSection } from "./components/pages/home/hero-section"
+import { KnownTechs } from "./components/pages/home/known-section"
 
 export default async function Home() {
   return (
     <>
       <HeroSection/>
+      <KnownTechs/>
     </>
   )
 }
