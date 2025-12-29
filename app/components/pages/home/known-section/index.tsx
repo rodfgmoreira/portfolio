287baa43a466132cab07da0c7b42098f3b0fc4a0
Rodrigo Moreira <rodfgmoreira@gmail.com>
2025-12-29 01:38:43 -0300

    feat: criado knowTech

--- app/components/pages/home/known-section/index.tsx
+++ app/components/pages/home/known-section/index.tsx
@@ -1,9 +1,20 @@
 import { SectionTitle } from "@/app/components/section-title";
+import { KnownTech } from "./know-tech";
+import { TbBrandNextjs } from "react-icons/tb";
 
 export function KnownTechs() {
   return (
     <section className="container py-16">
       <SectionTitle subtitle="competências" title="Conhecimentos" />
+
+      <div>
+        <KnownTech tech={{
+          icon: <TbBrandNextjs/>,
+          name: 'Next.js',
+          startDate: '2025-01-01'
+        }}
+        />
+      </div>
     </section>
   )
 }
