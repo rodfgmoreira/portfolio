9fff3d50561867b3e9428cd1a6192529ddc7106e
Rodrigo Moreira <rodfgmoreira@gmail.com>
2025-12-31 18:25:29 -0300

    feat: finalizado area de projetos

--- app/components/pages/home/highlighted-projects/index.tsx
+++ app/components/pages/home/highlighted-projects/index.tsx
@@ -1,6 +1,8 @@
 import { HorizontalDivider } from "@/app/components/divider/horizontal";
 import { SectionTitle } from "@/app/components/section-title";
 import { ProjectCard } from "./project-card";
+import { Link } from "@/app/components/link";
+import { HiArrowNarrowRight } from "react-icons/hi";
 
 export function HighlightedProjects() {
   return (
@@ -10,7 +12,17 @@ export function HighlightedProjects() {
 
       <div>
         <ProjectCard />
+        <HorizontalDivider className="my-16" />
         <ProjectCard />
+        <HorizontalDivider className="my-16" />
+
+        <p className="flex items-center gap-1.5">
+          <span className="text-gray-400">Se interessou?</span>
+          <Link className="inline-flex" href="/projects">
+            Ver todos
+            <HiArrowNarrowRight />
+          </Link>
+        </p>
       </div>
     </section>
   )
--- app/components/pages/home/highlighted-projects/project-card/index.tsx
+++ app/components/pages/home/highlighted-projects/project-card/index.tsx
@@ -1,26 +1,45 @@
+import { Link } from "@/app/components/link";
+import { TechBadge } from "@/app/components/tech-badge";
 import Image from "next/image";
+import { HiArrowNarrowRight } from "react-icons/hi";
 
 export function ProjectCard() {
   return (
-    <div>
+    <div className="flex gap-6 lg:gap-12 flex-col lg:flex-row">
       <div className="w-full h-full">
         <Image
           width={420}
           height={304}
           src="https://media.graphassets.com/FRhUdgUQTHmLmwf9u0BA"
           alt="Thumbnail de projeto"
-          className="object-cover rounded-lg"
+          className="w-full h-[200px] sm:h-[300px] lg:w-[420px] lg:min-h-full object-cover rounded-lg"
         />
       </div>
       <div>
-        <h3>
+        <h3 className="flex items-center gap-2 font-medium text-lg text-gray-50">
           <Image
             width={20}
             height={20}
             alt=""
             src="/images/icons/project-title-icon.svg"
           />
+          BookWise
         </h3>
+
+        <p className="text-gray-400 my-6">
+          Lorem ipsum dolor sit amet consectetur, adipisicing elit. Atque, ipsa odio nam quia eveniet odit incidunt facilis earum dignissimos dolor animi, sapiente, tempora tempore dolorum? Exercitationem doloribus quia asperiores sed dolorum! Minus aut corrupti commodi deserunt dolore repudiandae consectetur quasi quod in praesentium maiores ipsam tempora sit, suscipit doloremque ex.
+        </p>
+
+        <div className="flex gap-x-2 gap-y-3 flex-wrap mb-8 lg:max-w-[350px]">
+          <TechBadge name="Next.js"/>
+          <TechBadge name="Next.js"/>
+          <TechBadge name="Next.js"/>
+        </div>
+
+        <Link href="/projects/book-wise">
+          Ver projeto
+          <HiArrowNarrowRight />
+        </Link>
       </div>
     </div>
   )
