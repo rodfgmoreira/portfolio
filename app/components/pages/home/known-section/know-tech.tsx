cb3ad0b6eb7ebd04f74e308c6158db1513b4b235
Rodrigo Moreira <rodfgmoreira@gmail.com>
2025-12-31 18:26:30 -0300

    feat: Ajsutado cor do hover

--- app/components/pages/home/known-section/know-tech.tsx
+++ app/components/pages/home/known-section/know-tech.tsx
@@ -15,7 +15,7 @@ export function KnownTech({ tech }: KnownTechProps) {
     'pt-BR',
   ).replace('há ', '')
   return (
-    <div className="p-6 rounded-lg bg-gray-600/20 text-gray-500 flex flex-col gap-2 hover:text-emerald-500 hover:bg-gray-600/30 transition-all">
+    <div className="p-6 rounded-lg bg-gray-600/20 text-gray-500 flex flex-col gap-2 hover:text-cyan-500 hover:bg-gray-600/30 transition-all">
       <div className="flex items-center justify-between">
         <p>{tech.name}</p>
         {tech.icon}
