611a38fb8d47b26e353cd0872479631ff33fe3db
Rodrigo Moreira <rodfgmoreira@gmail.com>
2026-01-04 15:55:52 -0300

    feat: Alterado icone de projeto e inicio sessão de experiência

---
+++ app/components/pages/home/work-experience/experience-item.tsx
@@ -0,0 +1,7 @@
+export function ExperienceItem() {
+  return (
+    <div className="grid grid-cols-[40px,1fr] gap-4 md:;gap-10">
+
+    </div>
+  )
+}
---
+++ app/components/pages/home/work-experience/index.tsx
@@ -0,0 +1,18 @@
+import { SectionTitle } from "@/app/components/section-title";
+
+export function WorkExperience() {
+  return (
+    <section className="container py-16 flex gap-16">
+      <div className="max-w-[420px]">
+        <SectionTitle subtitle="experiências" title="Experiência Profissional"/>
+        <p className="text-gray-400 mt-6">
+          Estou sempre aberto a novos desafios e projetos emocionantes. Vamos trabalhar juntos para criar soluções incríveis para sua empresa!
+        </p>
+      </div>
+      
+      <div className="flex flex-col gap-4">
+        
+      </div>
+    </section>
+  )
+}
